1901cecb4e4b734adedcfeaeabd4760650fe89ee
hassen hosni <hosni.hassen@vistaprint.com>
2018-09-25 13:34:39 +0200

    chart is woring , dynamic left

--- src/app/chart-ping/chart-ping.component.ts
+++ src/app/chart-ping/chart-ping.component.ts
@@ -1,27 +1,116 @@
-import { Component, OnInit } from '@angular/core';
+import { Ping } from '../ping-list/ping';
+import { Component, OnInit,AfterViewInit, ElementRef, ViewChild} from '@angular/core';
 import { ApiService } from  '../api.service';
+import {Chart} from 'chart.js';
+
 @Component({
   selector: 'app-chart-ping',
   templateUrl: './chart-ping.component.html',
-  styleUrls: ['./chart-ping.component.css','assets/css/main.css' 
+  styleUrls: ['./chart-ping.component.css','assets/css/main.css'
   ,'assets/css/noscript.css']
 })
 export class ChartPingComponent implements OnInit {
-  private  ping:  Array<object> = [];
+  @ViewChild('canvas') canvas: ElementRef;
+  private  ping:  Array<string> = [];
+  private  pings:  Array<Ping> = [];
+  dates = [''];
+  averages = [1];
+chart: [''];
+
   constructor(private apiService : ApiService) { }
 
   ngOnInit() {
-    
+
+this.getAllpingchart();
+
   }
   public  getAllpingchart(){
-    var  user  = {
-      name:  "gnar",
-      mac:"526"
-  };
-    this.apiService.getAllpingchart(user).subscribe((data:  Array<object>) => {
-        this.ping  =  data;
-        console.log(data);
-        
+     this.chart = new Chart(this.canvas.nativeElement.getContext('2d'),{
+      type: 'line',
+      data: {
+          labels :this.dates,
+          datasets: [
+            {
+              data : this.averages,
+              borderColor : '#666666',
+              fill : true
+            },
+          ]
+      },
+      options: {
+        scales : {
+          xAxes: [{
+            display: true
+          }],
+          yAxes : [{
+            display : true
+          }]
+        }
+      }
     });
+    this.apiService.getAllping().subscribe((data:  Array<string>) => {
+        this.ping  =  data;
+
+        console.log("our data "+this.ping);
+        var i = 0;
+        this.ping.forEach(element => {//console.log(element);
+
+
+          var j = JSON.stringify(element);
+          //console.log(j);
+          var a=new Ping("0","0","0");
+          try{
+          var x = j.indexOf("average=");
+          var y = j.indexOf(",",x);
+          a.average=j.substring(x+8,y);
+
+            this.averages.push(Number(j.substring(x+8,y)));
+
+        }
+        catch(err){
+          console.log(err);
+        }
+        try {
+          x = j.indexOf("loss=")
+          var y = j.indexOf(",",x);
+          a.loss=j.substring(x+5,y);
+
+        } catch (error) {
+
+        }
+         try {
+          x=j.indexOf("date=")
+          y=j.indexOf(",",x);
+          a.date=j.substring(x+5,y);
+          this.dates.push(j.substring(x+5,y));
+
+         } catch (error) {
+
+         }
+
+          this.pings[i]=a;
+          //this.dates.push(pings[i].)
+          //console.log(this.pings[i]);
+          i=i+1;
+          var ch = new Chart(this.canvas.nativeElement.getContext('2d'));
+          console.log(ch);
+          //ch.Line(this.averages);
+         // this.ch.data.datasets.array.forEach((dataset) => {
+             // dataset.data.push(this.averages);
+
+          //  });
+          console.log("our dates"+this.dates+this.averages);
+        });
+
+       // console.log("our dates"+this.dates+this.averages);
+     //adding realtime
+        //  this.chart.data.datasets.array.forEach((dataset) => {
+        //  dataset.data.push(this.averages);
+
+       // });
+    });
+
+    //console.log("our dates"+this.dates+this.averages);
+
 }
 }
--- src/app/ping-list/ping-list.component.ts
+++ src/app/ping-list/ping-list.component.ts
@@ -1,16 +1,83 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from  '../api.service';
 import { Ping } from './ping';
+import { MessagingService } from "../services/messaging.service";
+import { Message } from "@stomp/stompjs";
+import { StompState } from "@stomp/ng2-stompjs";
+
+const WEBSOCKET_URL = "ws://localhost:1330/socket";
+const EXAMPLE_URL = "/topic/server-broadcaster";
+
 @Component({
   selector: 'app-ping-list',
   templateUrl: './ping-list.component.html',
-  styleUrls: ['./ping-list.component.css','assets/css/main.css' 
+  styleUrls: ['./ping-list.component.css','assets/css/main.css'
   ,'assets/css/noscript.css']
 })
 export class PingListComponent implements OnInit {
+  private messagingService: MessagingService;
+  public msg = "";
+  messageHistory = [];
+  state: string = "NOT CONNECTED";
+  private  pingss:  Array<Ping> = [];
+
+
   private  pings:  Array<Ping> = [];
+
   private test : Array<string>=[];
-  constructor(private  apiService:  ApiService) { }
+  constructor(private  apiService:  ApiService) {
+
+    // Instantiate a messagingService
+    this.messagingService = new MessagingService(WEBSOCKET_URL, EXAMPLE_URL);
+
+    // Subscribe to its stream (to listen on messages)
+    this.messagingService.stream().subscribe((message: Message) => {
+      this.messageHistory.unshift(message.body);
+
+
+       this.msg = JSON.stringify(message.body);
+      //console.log(j);
+      var a=new Ping("0","0","0");
+      try{
+      var x = this.msg.indexOf("average=");
+      var y = this.msg.indexOf(",",x);
+      a.average=this.msg.substring(x+8,y);
+
+    }
+    catch(err){
+      console.log(err);
+    }
+    try {
+      x = this.msg.indexOf("loss=")
+      var y = this.msg.indexOf(",",x);
+      a.loss=this.msg.substring(x+5,y);
+
+    } catch (error) {
+
+    }
+     try {
+      x=this.msg.indexOf("date=")
+      y=this.msg.indexOf(",",x);
+      a.date=this.msg.substring(x+5,y);
+
+     } catch (error) {
+
+     }
+
+      this.pingss.unshift(a);
+
+      console.log(message);
+    });
+
+    // Subscribe to its state (to know its connected or not)
+    this.messagingService.state().subscribe((state: StompState) => {
+      this.state = StompState[state];
+    });
+
+
+
+
+   }
 
   ngOnInit() {
     this.getAllping();
@@ -20,8 +87,8 @@ export class PingListComponent implements OnInit {
         this.test  =  data;
         var i = 0;
         this.test.forEach(element => {//console.log(element);
-          
-          
+
+
           var j = JSON.stringify(element);
           //console.log(j);
           var a=new Ping("0","0","0");
@@ -29,7 +96,7 @@ export class PingListComponent implements OnInit {
           var x = j.indexOf("average=");
           var y = j.indexOf(",",x);
           a.average=j.substring(x+8,y);
-          
+
         }
         catch(err){
           console.log(err);
@@ -38,23 +105,23 @@ export class PingListComponent implements OnInit {
           x = j.indexOf("loss=")
           var y = j.indexOf(",",x);
           a.loss=j.substring(x+5,y);
-           
+
         } catch (error) {
-          
+
         }
          try {
           x=j.indexOf("date=")
           y=j.indexOf(",",x);
           a.date=j.substring(x+5,y);
-           
+
          } catch (error) {
-           
+
          }
-          
+
           this.pings[i]=a;
           i=i+1;
          // console.log(this.users[i]);
-        });  
+        });
         console.log(data);
     });
 }
